e10b0b528b6eb46c549fbe125d20ae202767ee58
Vincent Franco <vince@freshivore.net>
2014-06-16 16:20:06 -0700

    Adding some more feature specs

--- spec/features/admin_spec.rb
+++ spec/features/admin_spec.rb
@@ -1,17 +1,30 @@
 require 'spec_helper'
 
-feature 'Stylesheet Admin' do
+feature 'Stylesheet administration' do
   stub_authorization!
 
-  given!(:stylesheet) { create(:stylesheet) }
+  let(:show_path) { spree.admin_stylesheet_path }
 
-  context 'show' do
-    background do
-      visit spree.admin_stylesheet_path
-    end
+  scenario 'user views page with no stylesheet' do
+    visit show_path
+    expect(page).to have_text 'No custom style yet!'
+    expect(page).to_not have_text 'Revision history'
+  end
 
-    scenario 'shows style' do
-      expect(page).to have_text stylesheet.style_raw
-    end
+  scenario 'user views page with existing stylesheet' do
+    style = create :stylesheet
+    visit show_path
+
+    expect(page).to have_text style.style_raw
+    expect(page).to_not have_text 'Revision history'
+  end
+
+  scenario 'user views page with stylesheet history' do
+    new_styles = 'body {display: none;}'
+    create(:stylesheet).update_attributes style_raw: new_styles
+    visit show_path
+
+    expect(page).to have_text new_styles
+    expect(page).to have_text 'Revision history'
   end
 end

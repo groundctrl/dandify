16f1937e0ea136d2198ef43f51c124699cc3d20d
Vincent Franco <vince@freshivore.net>
2014-06-18 17:27:57 -0700

    Back to red

--- spec/features/editing_stylsheet_spec.rb
+++ spec/features/editing_stylsheet_spec.rb
@@ -4,6 +4,7 @@ feature 'Managing stylesheets' do
   stub_authorization!
 
   let(:show_path) { spree.admin_stylesheet_path }
+  let(:new_path) { spree.new_admin_stylesheet_path }
   let(:edit_path) { spree.edit_admin_stylesheet_path }
 
   context 'without existing style' do
@@ -11,6 +12,12 @@ feature 'Managing stylesheets' do
       visit show_path
       click_link 'Create style'
       expect(page).to have_text 'Creating Custom Styles'
+    end
+
+    scenario 'user can create styles' do
+      visit new_path
+      fill_in 'Style raw', with: 'body {display: none;}'
+      click_button 'Create Stylesheet'
     end
   end
 

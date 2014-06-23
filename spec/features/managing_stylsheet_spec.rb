8170499764e1bd2eec833d769a33fedbdc48cca8
Vincent Franco <vince@freshivore.net>
2014-06-23 11:40:27 -0700

    Adding some more integration tests

--- spec/features/managing_stylsheet_spec.rb
+++ spec/features/managing_stylsheet_spec.rb
@@ -11,16 +11,36 @@ feature 'Managing stylesheets' do
     scenario 'user can navigate to edit screen' do
       visit show_path
       click_link 'Create style'
+
       expect(page).to have_text 'Creating Custom Styles'
     end
 
     scenario 'user can create styles' do
       visit new_path
-      fill_in 'Style Raw', with: 'body {display: none;}'
+      fill_in 'Styles', with: 'body {display: none;}'
       click_button 'Create'
+
+      expect(page).to have_text 'body {display: none;}'
     end
   end
 
   context 'with existing style' do
+    before { create :stylesheet }
+
+    scenario 'user can edit exiting styles' do
+      visit show_path
+      click_link 'Edit Style'
+
+      expect(page).to have_text 'Editing Custom Styles'
+      expect(page).to have_text Spree::Stylesheet.first.style_raw
+    end
+
+    scenario 'user creates audit trail on edit', versioning: true do
+      visit edit_path
+      fill_in 'Styles', with: '#main {display: none;}'
+      click_button 'Update'
+
+      expect(page).to have_text 'Revision history'
+    end
   end
 end

ce1f4fcd23990d85ebe684133ad7deec0688f3a4
Vincent Franco <vince@freshivore.net>
2014-06-23 11:55:14 -0700

    A bit more front facing coverage

--- spec/controllers/spree/stylesheets_controller_spec.rb
+++ spec/controllers/spree/stylesheets_controller_spec.rb
@@ -1,33 +1,36 @@
 require 'spec_helper'
 
-describe Spree::StylesheetsController do
-  describe '#show' do
-    context 'with a bad format' do
-      it 'still responds with blank sheet' do
-        spree_get :show, format: :xml
-        expect(response.header['Content-Type']).to include 'text/css'
-      end
+describe Spree::StylesheetsController, '#show' do
+  context 'with a bad format' do
+    it 'responds with blank sheet' do
+      spree_get :show, format: :xml
 
-      it 'still responds with found sheet' do
-        create(:stylesheet).style_compressed
-        spree_get :show, format: :atom
-        expect(response.header['Content-Type']).to include 'text/css'
-      end
+      expect(response.header['Content-Type']).to include 'text/css'
     end
 
-    context 'when there is no stylesheet' do
-      it 'displays a blank stylesheet' do
-        spree_get :show, format: :css
-        expect(assigns[:style]).to be_a Spree::BlankStylesheet
-      end
-    end
+    it 'responds with found sheet' do
+      css = create(:stylesheet).style_compressed
+      spree_get :show, format: :atom
 
-    context 'when there is a stylesheet' do
-      it 'assigns the stored sheet' do
-        create :stylesheet
-        spree_get :show, format: :css
-        expect(assigns[:style]).to be_a Spree::Stylesheet
-      end
+      expect(response.header['Content-Type']).to include 'text/css'
+      expect(assigns[:style].style_compressed).to eq css
+    end
+  end
+
+  context 'when there is no stylesheet' do
+    it 'displays a blank stylesheet' do
+      spree_get :show, format: :css
+
+      expect(assigns[:style]).to be_a Spree::BlankStylesheet
+    end
+  end
+
+  context 'when there is a stylesheet' do
+    it 'assigns the stored sheet' do
+      create :stylesheet
+      spree_get :show, format: :css
+
+      expect(assigns[:style]).to be_a Spree::Stylesheet
     end
   end
 end
